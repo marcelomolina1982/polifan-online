355de7890ee85728bd84ceaa3c6f51b743a41a72
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-06 15:28:20 -0300

    Add files via upload

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='18.6.1'
-export const APP_VERSION_LABEL='18.6.1'
+export const APP_VERSION='18.6.2'
+export const APP_VERSION_LABEL='18.6.2'
 export const APP_UPDATED_AT='06/08/2026'
-export const APP_VERSION_NAME='Avatar grande apoyado sobre el botón'
+export const APP_VERSION_NAME='Asistente compacto abajo a la derecha'
