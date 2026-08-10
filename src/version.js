56e83c9b2c348083aadef032cac066dbee0447f0
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 16:28:55 -0300

    Version 24.4.0 motor NFP

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='24.3.5'
-export const APP_VERSION_LABEL='v24.3.5'
+export const APP_VERSION='24.4.0'
+export const APP_VERSION_LABEL='v24.4.0'
 export const APP_UPDATED_AT='10/08/2026'
-export const APP_VERSION_NAME='Motor V5.1 geométrico · rotaciones reales · diversidad espacial · V1.7 certifica'
+export const APP_VERSION_NAME='Motor NFP · Minkowski/Clipper · kits completos · V1.7 certifica'
