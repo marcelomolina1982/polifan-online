3d8a3d9f6b6eb4f29846e564b4dbd5184125ff33
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 08:50:28 -0300

    Cargar mejoras visuales y de catálogo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './trust-overrides.css'
+import './catalog-pro.css'
 import './trustCatalogEnhancements'
+import './catalogProEnhancements'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
