4d38628eb0dae6b183bad95332314f2531ffaefc
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 11:35:07 -0300

    Version 24.2.2 una placa por vez

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='24.2.1'
-export const APP_VERSION_LABEL='v24.2.1'
+export const APP_VERSION='24.2.2'
+export const APP_VERSION_LABEL='v24.2.2'
 export const APP_UPDATED_AT='10/08/2026'
-export const APP_VERSION_NAME='Generador automático · reconocimiento flexible de Biblioteca SVG'
+export const APP_VERSION_NAME='Una placa por vez · prepacking seguro'
