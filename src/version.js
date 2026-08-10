c7cb33303fac4f88a44063a5d01245bc1ef1db8b
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 09:35:41 -0300

    Version 23.4.1 corregir flujo generar placas

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='23.4.0'
-export const APP_VERSION_LABEL='v23.4.0'
+export const APP_VERSION='23.4.1'
+export const APP_VERSION_LABEL='v23.4.1'
 export const APP_UPDATED_AT='10/08/2026'
-export const APP_VERSION_NAME='Motor Polifan Definitivo V1.7 integrado'
+export const APP_VERSION_NAME='Generar placas con Motor V1.7 · flujo corregido'
