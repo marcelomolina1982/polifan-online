a5de8863b6454459bb27c05f461fe3c9d05bae4c
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 08:53:54 -0300

    Actualizar versión con Motor Definitivo V1.7

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='23.3.0'
-export const APP_VERSION_LABEL='v23.3.0'
-export const APP_UPDATED_AT='09/08/2026'
-export const APP_VERSION_NAME='Mejoras operativas · catálogo y clientes'
+export const APP_VERSION='23.4.0'
+export const APP_VERSION_LABEL='v23.4.0'
+export const APP_UPDATED_AT='10/08/2026'
+export const APP_VERSION_NAME='Motor Polifan Definitivo V1.7 integrado'
