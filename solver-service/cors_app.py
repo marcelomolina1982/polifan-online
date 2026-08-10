e693f6fe43004e557326161ea7008d7ce42d48d5
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 13:50:26 -0300

    Habilitar CORS para Motor industrial V2

---
+++ solver-service/cors_app.py
@@ -0,0 +1,5 @@
+from extended_app import app
+from flask_cors import CORS
+
+# Permitir que la app web en Vercel llame directamente al solver de Render.
+CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers=["Content-Type"], methods=["GET", "POST", "OPTIONS"])
