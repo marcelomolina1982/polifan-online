7d5c879821694517240220763b2b543b2ee91e88
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-09 21:41:41 -0300

    Bump version for order profitability view

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='23.4.0'
-export const APP_VERSION_LABEL='v23.4.0'
+export const APP_VERSION='23.4.1'
+export const APP_VERSION_LABEL='v23.4.1'
 export const APP_UPDATED_AT='09/08/2026'
-export const APP_VERSION_NAME='Costos reales · embalaje y ganancia por pedido'
+export const APP_VERSION_NAME='Rentabilidad visible en pedidos'
