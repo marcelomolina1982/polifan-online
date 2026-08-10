6be3e7d23d1384cdd8e3086d53d3dcedc3002178
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 10:46:52 -0300

    v24.2.1 reconocer alias SVG en generador automático

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='24.2.0'
-export const APP_VERSION_LABEL='v24.2.0'
+export const APP_VERSION='24.2.1'
+export const APP_VERSION_LABEL='v24.2.1'
 export const APP_UPDATED_AT='10/08/2026'
-export const APP_VERSION_NAME='Generador automático de placas · Motor Definitivo V1.7'
+export const APP_VERSION_NAME='Generador automático · reconocimiento flexible de Biblioteca SVG'
