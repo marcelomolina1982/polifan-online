07a29793f64f7a3ae9686a5f085fb305c9f70760
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-09 21:46:03 -0300

    Actualizar versión fotos y reseñas

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='23.4.1'
-export const APP_VERSION_LABEL='v23.4.1'
+export const APP_VERSION='23.4.2'
+export const APP_VERSION_LABEL='v23.4.2'
 export const APP_UPDATED_AT='09/08/2026'
-export const APP_VERSION_NAME='Rentabilidad visible en pedidos'
+export const APP_VERSION_NAME='Fotos compactas · imágenes en reseñas'
