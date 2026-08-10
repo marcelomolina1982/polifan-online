27e1bd6a84649e2f6c9cda2919e5491559ed8f3c
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-10 08:52:35 -0300

    Actualizar versión a v24.0.0

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='23.5.0'
-export const APP_VERSION_LABEL='v23.5.0'
-export const APP_UPDATED_AT='09/08/2026'
-export const APP_VERSION_NAME='Pedido unificado · despacho y productos manuales'
+export const APP_VERSION='24.0.0'
+export const APP_VERSION_LABEL='v24.0.0'
+export const APP_UPDATED_AT='10/08/2026'
+export const APP_VERSION_NAME='Centro operativo · catálogo pro · costos editables'
