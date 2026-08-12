99be8ce922c9147009fef8c56a5be6e00293d8d8
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-12 12:52:27 -0300

    lab: abrir directamente la interfaz aislada

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import LabApp from './LabApp'
 import './styles.css'
 import './trust-overrides.css'
 import './catalog-pro.css'
-import './trustCatalogEnhancements'
-import './catalogProEnhancements'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><LabApp /></React.StrictMode>
 )
