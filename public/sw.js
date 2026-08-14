05c69f217178722ada0ff13ffd2bcc19cdcb1ccf
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-14 13:16:55 -0300

    forzar carga inmediata de la correccion de pedidos

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION='15.3'
+const SW_VERSION='15.4'
 self.addEventListener('install',event=>{self.skipWaiting()})
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(key=>caches.delete(key)));await self.clients.claim()})())})
 self.addEventListener('fetch',event=>{
