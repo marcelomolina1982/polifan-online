6871efae8f5ddc4771e87c0ef9063ace328243cc
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-14 13:36:55 -0300

    forzar carga de persistencia durable de pedidos

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION='15.4'
+const SW_VERSION='15.5'
 self.addEventListener('install',event=>{self.skipWaiting()})
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(key=>caches.delete(key)));await self.clients.claim()})())})
 self.addEventListener('fetch',event=>{
