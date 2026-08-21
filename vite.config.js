013469b96d2a76b284b201f9a04c4446e708f8f7
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-21 00:42:57 -0300

    Enable Case 9 complete-kit guard

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import case9GuardPlugin from './case9GuardPlugin.js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [case9GuardPlugin(), react()],
 })
