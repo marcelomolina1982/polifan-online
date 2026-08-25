6b771d34ed15cdb5d10cae11754264a1e6481668
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-25 12:34:55 -0300

    chore: forzar actualización clientes v25.0.39

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION='25.0.35'
+const SW_VERSION='25.0.39'
 self.addEventListener('install',event=>{self.skipWaiting()})
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{
   const keys=await caches.keys()
