d4168ca0e08f86d57cf497d53ece9a07ebc589d1
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-25 01:00:23 -0300

    chore: force refresh for stock recovery fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION='25.0.34'
+const SW_VERSION='25.0.35'
 self.addEventListener('install',event=>{self.skipWaiting()})
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{
   const keys=await caches.keys()
