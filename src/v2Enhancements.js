ad21bc502957e9b58b58bc867d849f852730b8ad
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-08-27 15:35:54 -0300

    refactor(v2): centralize progressive enhancement imports

---
+++ src/v2Enhancements.js
@@ -0,0 +1,9 @@
+// Puente único para mejoras progresivas V2 que todavía viven fuera del árbol React.
+// Centralizarlas acá mantiene main.jsx limpio y facilita retirarlas a medida que cada mejora migre a componentes nativos.
+import './v2CommandCenter'
+import './v2FlowUx'
+import './v2InventoryAudit'
+import './v2AuditTrail'
+import './trustCatalogEnhancements'
+import './catalogProEnhancements'
+import './orders-v2-enhancements'
