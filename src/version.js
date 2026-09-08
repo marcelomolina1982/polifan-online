9d873e7a4b267d614448946d218152bc20641194
marcelomolina1982 <marcelo.molina1982@gmail.com>
2026-09-08 17:22:17 -0300

    Version final seguimiento operativo

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='25.0.91'
-export const APP_VERSION_LABEL='v25.0.91'
+export const APP_VERSION='25.0.92'
+export const APP_VERSION_LABEL='v25.0.92'
 export const APP_UPDATED_AT='08/09/2026'
 export const APP_VERSION_NAME='Polifan 25 · seguimiento operativo consolidado'
